0bbc5e60d0e299acee73a1c3bd1910ca64912f06
tom <tom.nota.pro@gmail.com>
2024-12-28 19:03:44 +0100

    yes

--- js/mangas2.js
+++ js/mangas2.js
@@ -1,6 +1,3 @@
-// Création des éléments HTML
-const upDiv = document.createElement('div');
-upDiv.id = 'up';
 
 const navbarDiv = document.createElement('div');
 navbarDiv.id = 'navbar';
@@ -8,21 +5,10 @@ navbarDiv.id = 'navbar';
 const mangasList = document.createElement('main');
 mangasList.id = 'mangas-list';
 
-const upButton = document.createElement('a');
-upButton.href = '#up';
-upButton.id = 'upButton';
-const upImage = document.createElement('img');
-upImage.className = 'up';
-upImage.src = 'images/up.png'; // Remplace par ton image
-upImage.alt = '404';
-upButton.appendChild(upImage);
-
 const footerContainer = document.createElement('div');
 footerContainer.id = 'footer-container';
 
 // Ajout des éléments au body
-document.body.appendChild(upDiv);
 document.body.appendChild(navbarDiv);
 document.body.appendChild(mangasList);
-document.body.appendChild(upButton);
 document.body.appendChild(footerContainer);
